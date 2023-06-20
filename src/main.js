01c790be0710207e443302543544946df3fb7136
bhunk <bhunkin@live.com>
2023-06-20 00:47:55 -0400

    imageURLs updated + little code fixes + cleanup

--- src/main.js
+++ src/main.js
@@ -1,23 +1,47 @@
 import Phaser from 'phaser';
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var config = {
+    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+
+var game = new Phaser.Game(config);
 
 function preload() {
-
+    console.log('preload');
     //  You can fill the preloader with as many assets as your game requires
 
     //  Here we are loading an image. The first parameter is the unique
     //  string by which we'll identify the image later in our code.
 
     //  The second parameter is the URL of the image (relative)
-    game.load.image('einstein', "C://phaser_project//src//download.png");
+    //this.load.baseURL = 'src/';
+    this.load.setBaseURL('http://localhost:1234');
+    this.load.image('bob', "https://raw.githubusercontent.com/bhunk/towerdeez/master/src/assets/sprite.png");
+    console.log('post-preload');
 
 }
 
 function create() {
-
+    console.log('create');
     //  This creates a simple sprite that is using our loaded image and
     //  displays it on-screen
-    game.add.sprite(0, 0, 'einstein');
+  //  this.add.sprite(0, 0, 'einstein');
+  //  this.bob = this.add.image(10,10,'bob');
+    this.add.image(200,200,'bob');
+    //this.image.preload.add.image(0,0,'bob');
+    console.log('after image add')
 
 }
